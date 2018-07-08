9a5578d375ffcf4a8714fb90a737ed144812f975
MS <mixa.ru.ru@gmail.com>
2018-07-08 12:01:52 +0300

    Drpzone handler: upload multiple files at once

--- src/Controller/Admin/GalleryController.php
+++ src/Controller/Admin/GalleryController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class GalleryController extends Controller
 {
@@ -17,8 +19,23 @@ class GalleryController extends Controller
      *
      * @return Response
      */
-    public function profileAction(Request $request)
+    public function new(Request $request)
     {
         return $this->render('admin/pages/gallery/new.html.twig', []);
+    }
+
+
+    /**
+     * @Route("/upload", name="_gallery_upload")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function uploadHandler(Request $request)
+    {
+        $files = $request->files->all();
+        var_dump($files);
+        exit();
     }
 }
